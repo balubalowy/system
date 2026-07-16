426cdf8ad98e415099e4a693f8a486ccef2226e5
B-Core <bartek@system.local>
2026-07-16 17:02:40 +0200

    Aktualizacja z dysku

--- app/js/local_data.js
+++ app/js/local_data.js
@@ -1,1 +1,1 @@
-﻿window.localAgentStats = { photos: 1870, excels: 20, storms: 0, lastSync: '2026-07-16 16:54' };
+﻿window.localAgentStats = { photos: 1870, excels: 20, storms: 0, lastSync: '2026-07-16 17:02' };
