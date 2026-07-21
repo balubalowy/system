5832cb926836f88acd997dfd691f9ccac096f6e2
B-Core <bartek@system.local>
2026-07-21 13:17:08 +0200

    auto: aktualizacja statystyk i pamietnika burz

--- app/js/local_data.js
+++ app/js/local_data.js
@@ -1,1 +1,1 @@
-﻿window.localAgentStats = { photos: 0, excels: 0, storms: 0, stormHunts: 84, stormDaysHunts: 59, stormTotal: 273, stormDaysTotal: 170, stormKm: 5222, lastSync: '2026-07-21 13:07' };
+﻿window.localAgentStats = { photos: 1870, excels: 21, storms: 19, stormHunts: 84, stormDaysHunts: 59, stormTotal: 273, stormDaysTotal: 170, stormKm: 5222, lastSync: '2026-07-21 13:17' };
